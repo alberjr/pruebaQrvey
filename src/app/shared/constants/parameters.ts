29439496f9249ca6d3755961895712f8c21202c5
Alber Barros <adbarros@bancolombia.com.co>
2021-10-08 18:20:28 -0500

    se configura sass, se consume el servicio y se hace primera configuracion de las rutas

---
+++ src/app/shared/constants/parameters.ts
@@ -0,0 +1,7 @@
+
+export const parametros = {
+    countryService: 'all',
+    delayGeneric: 1000,
+    timeoutGeneric: 4000,
+    numRetriesServices: 3
+}
---
+++ src/app/shared/services/country-catalogs.service.spec.ts
@@ -0,0 +1,62 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, of,throwError } from 'rxjs';
+import { map, catchError, timeout, retryWhen, delay, take, tap } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { parametros } from '../constants/parameters';
+
+@Injectable()
+export class CreditRequestService {
+
+    private endpoint = environment.serverUrl;
+    private failuresNum = 0;
+    
+    constructor(
+        private httpClient: HttpClient) {
+    }
+
+    getPromotion(): Observable<HttpResponse<any>> {
+        const url = this.endpoint + parametros.countryService;    
+
+        return this.httpClient.get<HttpResponse<any>>(url)
+        .pipe(
+            timeout(parametros.timeoutGeneric),
+            retryWhen(errors => errors.pipe(
+                delay(parametros.delayGeneric),
+                take(parametros.numRetriesServices),
+                tap(response => {
+                    if (response.status) {
+                        if ((response.status + '').startsWith('5') ||
+                            (response.status + '').startsWith('4')) {
+                            if ((response.status + '').startsWith('4')) {
+                                this.failuresNum = parametros.numRetriesServices;
+                            } else {
+                                this.failuresNum++;
+                            }
+                            if (this.failuresNum >= parametros.numRetriesServices) {
+                                throw response;
+                            }
+                        }
+                    }
+                })
+            )),
+            map((result: HttpResponse<any>) => {
+                return result;
+            }),
+            catchError(this.handleError)
+        );
+    }
+
+    handleError(error: any) {
+        let errorMessage = '';
+        if(error.error instanceof ErrorEvent) {
+          // Get client-side error
+          errorMessage = error.error.message;
+        } else {
+          // Get server-side error
+          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        window.alert(errorMessage);
+        return throwError(errorMessage);
+     }
+}
